9b777b92b1528d1f3fac89e2b8e056a9e8408ad4
longguanlin <1591116978@qq.com>
2023-07-29 15:15:31 +0800

    4.13集成Mybatis持久层框架

--- member/src/main/java/com/lgl/train/member/config/MemberApplication.java
+++ member/src/main/java/com/lgl/train/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
 package com.lgl.train.member.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.lgl")
+@MapperScan("com.lgl.train.member.mapper")
 public class MemberApplication {
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
---
+++ member/src/main/java/com/lgl/train/member/mapper/MemberMapper.java
@@ -0,0 +1,6 @@
+package com.lgl.train.member.mapper;
+
+public interface MemberMapper {
+
+    int count();
+}
