8be73f47bc07dda2154eddb94d8fd20e88932cea
longguanlin <1591116978@qq.com>
2023-07-29 23:13:50 +0800

    4封装统一返回结果和异常处理

--- member/src/main/java/com/lgl/train/member/controller/MemberController.java
+++ member/src/main/java/com/lgl/train/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.lgl.train.member.controller;
 
+import com.lgl.train.common.resp.CommonResp;
+import com.lgl.train.member.req.MemberRegisterReq;
 import com.lgl.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,19 @@ public class MemberController {
     private MemberService memberService;
 
     @GetMapping("/count")
-    public Integer hello(){return memberService.count();}
+    public CommonResp<Integer> count(){
+        int count = memberService.count();
+        CommonResp<Integer> commonResp = new CommonResp<>();
+        commonResp.setContent(count);
+        return commonResp;
+    }
+
+    @PostMapping("/register")
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
+        long register = memberService.register(req);
+        // CommonResp<Long> commonResp = new CommonResp<>();
+        // commonResp.setContent(register);
+        // return commonResp;
+        return new CommonResp<>(register);
+    }
 }
--- member/src/main/java/com/lgl/train/member/service/MemberService.java
+++ member/src/main/java/com/lgl/train/member/service/MemberService.java
@@ -1,8 +1,17 @@
 package com.lgl.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.lgl.train.common.exception.BusinessException;
+import com.lgl.train.common.exception.BusinessExceptionEnum;
+import com.lgl.train.common.util.SnowUtil;
+import com.lgl.train.member.domain.Member;
+import com.lgl.train.member.domain.MemberExample;
 import com.lgl.train.member.mapper.MemberMapper;
+import com.lgl.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +21,21 @@ public class MemberService {
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(MemberRegisterReq req){
+        String mobile = req.getMobile();
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if(CollUtil.isNotEmpty(list)){
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
+        }
+        Member member = new Member();
+        member.setId(SnowUtil.getSnowflakeNextId());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
