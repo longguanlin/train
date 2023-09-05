2ca6ba2ee23cccd5edb0034abcb43232dc389b47
longguanlin <1591116978@qq.com>
2023-09-05 22:43:23 +0800

    第15章 集成分布式事务组件Seata（解决分布式系统中的数据一致性问题）

--- member/src/main/java/com/lgl/train/member/service/TicketService.java
+++ member/src/main/java/com/lgl/train/member/service/TicketService.java
@@ -13,6 +13,7 @@ import com.lgl.train.member.domain.TicketExample;
 import com.lgl.train.member.mapper.TicketMapper;
 import com.lgl.train.member.req.TicketQueryReq;
 import com.lgl.train.member.resp.TicketQueryResp;
+import io.seata.core.context.RootContext;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +35,7 @@ public class TicketService {
      * @param req
      */
     public void save(MemberTicketReq req) throws Exception {
-        // LOG.info("seata全局事务ID save: {}", RootContext.getXID());
+        LOG.info("seata全局事务ID save: {}", RootContext.getXID());
         DateTime now = DateTime.now();
         Ticket ticket = BeanUtil.copyProperties(req, Ticket.class);
         ticket.setId(SnowUtil.getSnowflakeNextId());
