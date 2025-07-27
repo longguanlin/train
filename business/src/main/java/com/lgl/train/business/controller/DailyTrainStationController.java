278a07641fb0afc832b427436219b8982f037b3f
longguanlin <1591116978@qq.com>
2025-07-27 14:52:44 +0800

    第22-3、4、余票查询页面增加显示车站信息

---
+++ business/src/main/java/com/lgl/train/business/controller/DailyTrainStationController.java
@@ -0,0 +1,28 @@
+package com.lgl.train.business.controller;
+
+import com.lgl.train.business.req.DailyTrainStationQueryAllReq;
+import com.lgl.train.business.resp.DailyTrainStationQueryResp;
+import com.lgl.train.business.service.DailyTrainStationService;
+import com.lgl.train.common.resp.CommonResp;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/daily-train-station")
+public class DailyTrainStationController {
+
+    @Autowired
+    private DailyTrainStationService dailyTrainStationService;
+
+    @GetMapping("/query-by-train-code")
+    public CommonResp<List<DailyTrainStationQueryResp>> queryByTrain(@Valid DailyTrainStationQueryAllReq req) {
+        List<DailyTrainStationQueryResp> list = dailyTrainStationService.queryByTrain(req.getDate(), req.getTrainCode());
+        return new CommonResp<>(list);
+    }
+
+}
